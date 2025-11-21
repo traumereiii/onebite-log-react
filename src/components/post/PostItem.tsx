b1272a7b50d39094cc644e60afebc463cf4deae2
DongSeong Seo <sds901234@naver.com>
2025-11-21 10:44:06 +0900

    feat: 내 포스트에만 수정/삭제 버튼 렌더링하기

--- src/components/post/PostItem.tsx
+++ src/components/post/PostItem.tsx
@@ -10,8 +10,14 @@ import defaultAvatar from "@/assets/default-avatar.jpg";
 import { formatTimeAgo } from "@/lib/time.ts";
 import EditPostButton from "@/components/post/EditPostButton.tsx";
 import DeletePostButton from "@/components/post/DeletePostButton.tsx";
+import { useSession } from "@/store/session.ts";
 
 export default function PostItem(post: Post) {
+  const session = useSession();
+  const userId = session?.user.id;
+
+  const isMine = post.author_id === userId
+
   return (
     <div className="flex flex-col gap-4 border-b pb-8">
       {/* 1. 유저 정보, 수정/삭제 버튼 */}
@@ -35,8 +41,12 @@ export default function PostItem(post: Post) {
 
         {/* 1-2. 수정/삭제 버튼 */}
         <div className="text-muted-foreground flex text-sm">
-          <EditPostButton {...post} />
-          <DeletePostButton id={post.id} />
+          {
+            isMine && <>
+              <EditPostButton {...post} />
+              <DeletePostButton id={post.id} />
+            </>
+          }
         </div>
       </div>
 
