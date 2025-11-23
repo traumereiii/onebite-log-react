b11a33719d1486bb3c33470002f24e705496c7dc
lullaaaby13 <sds901234@naver.com>
2025-11-23 13:41:16 +0900

    feat: 댓글 캐시 업데이트 기능 수정

--- src/hooks/mutations/comment/use-create-comment.ts
+++ src/hooks/mutations/comment/use-create-comment.ts
@@ -1,12 +1,31 @@
-import { useMutation } from "@tanstack/react-query";
-import type { UseMutationCallback } from "@/types.ts";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Comment, UseMutationCallback } from "@/types.ts";
 import { createComment } from "@/api/comment.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
+import { useSession } from "@/store/session.ts";
+import { useProfileData } from "@/hooks/queries/use-profile-data.ts";
 
 export function useCreateComment(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+  const session = useSession();
+  const { data: profile } = useProfileData(session?.user.id);
+
   return useMutation({
     mutationFn: createComment,
-    onSuccess: () => {
+    onSuccess: (newComment) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(newComment.post_id),
+        (comments) => {
+          if (!comments)
+            throw new Error("댓글이 캐시 데이터에 보관 되어 있지 않습니다.");
+          if (!profile)
+            throw new Error("사용자 프로필 데이터를 불러올 수 없습니다.");
+
+          return [{ ...newComment, author: profile }, ...comments];
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
--- src/hooks/mutations/comment/use-delete-comment.ts
+++ src/hooks/mutations/comment/use-delete-comment.ts
@@ -1,12 +1,25 @@
-import { useMutation } from "@tanstack/react-query";
-import type { UseMutationCallback } from "@/types.ts";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Comment, UseMutationCallback } from "@/types.ts";
 import { deleteComment } from "@/api/comment.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
 export function useDeleteComment(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteComment,
-    onSuccess: () => {
+    onSuccess: (deleteComment) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(deleteComment.post_id),
+        (comments) => {
+          if (!comments)
+            throw new Error("댓글이 캐시 데이터에 보관 되어 있지 않습니다.");
+
+          return comments.filter((comment) => comment.id !== deleteComment.id);
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
